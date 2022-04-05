7e140dd714fcdb342e0712b366420f7de660c896
TanmayJain33 <tanmay.jain@crownstack.com>
2022-04-05 15:18:29 +0530

    Add useState() hook

--- App.js
+++ App.js
@@ -1,43 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, Button, TouchableOpacity} from 'react-native';
 
-class App extends React.Component {
-  state = {
-    counter: 0,
-    buttonEnabled: true,
-  };
-  render() {
-    const counter = this.state.counter;
-    const buttonColor = this.state.buttonEnabled ? 'red' : 'blue';
-    return (
-      <View style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
-        <Text style={{textAlign: 'center', fontSize: 50}}>{counter}</Text>
-        <Button
-          title="Increment"
-          onPress={() => {
-            this.setState({counter: counter + 1});
-          }}
-        />
-        <View style={{margin: 10}} />
-        <Button
-          title="Decrement"
-          onPress={() => {
-            this.setState({counter: counter - 1});
-          }}
-        />
-        <TouchableOpacity
-          style={{marginTop: 20, padding: 10, backgroundColor: buttonColor}}
-          onPress={() => {
-            this.setState({
-              counter: 0,
-              buttonEnabled: !this.state.buttonEnabled,
-            });
-          }}>
-          <Text style={{color: 'white', fontWeight: '700'}}>RESET</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-}
+export default function App() {
+  const [counter, setCounter] = useState(0);
+  const [buttonEnabled, setButtonEnabled] = useState(true);
+  const buttonColor = buttonEnabled ? 'red' : 'blue';
 
-export default App;
+  return (
+    <View style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
+      <Text style={{textAlign: 'center', fontSize: 50}}>{counter}</Text>
+      <Button
+        title="Increment"
+        onPress={() => {
+          setCounter(counter + 1);
+        }}
+      />
+      <View style={{margin: 10}} />
+      <Button
+        title="Decrement"
+        onPress={() => {
+          setCounter(counter - 1);
+        }}
+      />
+      <TouchableOpacity
+        style={{marginTop: 20, padding: 10, backgroundColor: buttonColor}}
+        onPress={() => {
+          setCounter(0);
+          setButtonEnabled(!buttonEnabled);
+        }}>
+        <Text style={{color: 'white', fontWeight: '700'}}>RESET</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
