93df532e2a3b5cf6bc11b0dbfa199af94aaee70d
TanmayJain33 <tanmay.jain@crownstack.com>
2022-04-05 17:07:55 +0530

    Add useState() with object

--- App.js
+++ App.js
@@ -1,49 +1,37 @@
 import React, {useState} from 'react';
-import {View, Text, Button, TouchableOpacity} from 'react-native';
+import {View, Text, TextInput} from 'react-native';
 
 export default function App() {
-  const [counter, setCounter] = useState(0);
-  const [buttonEnabled, setButtonEnabled] = useState(true);
-  const buttonColor = buttonEnabled ? 'red' : 'blue';
+  const [name, setName] = useState({firstName: '', lastName: ''});
 
   return (
     <View style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
-      <Text style={{textAlign: 'center', fontSize: 50}}>{counter}</Text>
-      <Button
-        title="Increment By 1"
-        onPress={() => {
-          setCounter(prevCounter => prevCounter + 1);
+      <TextInput
+        style={{
+          height: 40,
+          width: 200,
+          margin: 12,
+          borderWidth: 1,
+          padding: 10,
         }}
+        value={name.firstName}
+        placeholder="Enter your first name"
+        onChangeText={text => setName({...name, firstName: text})}
       />
-      <View style={{margin: 10}} />
-      <Button
-        title="Decrement By 1"
-        onPress={() => {
-          setCounter(prevCounter => prevCounter - 1);
+      <TextInput
+        style={{
+          height: 40,
+          width: 200,
+          margin: 12,
+          borderWidth: 1,
+          padding: 10,
         }}
+        value={name.lastName}
+        placeholder="Enter your last name"
+        onChangeText={text => setName({...name, lastName: text})}
       />
-      <View style={{margin: 10}} />
-      <Button
-        title="Increment By 5"
-        onPress={() => {
-          setCounter(prevCounter => prevCounter + 5);
-        }}
-      />
-      <View style={{margin: 10}} />
-      <Button
-        title="Decrement By 5"
-        onPress={() => {
-          setCounter(prevCounter => prevCounter - 5);
-        }}
-      />
-      <TouchableOpacity
-        style={{marginTop: 20, padding: 10, backgroundColor: buttonColor}}
-        onPress={() => {
-          setCounter(prevCounter => prevCounter - prevCounter);
-          setButtonEnabled(!buttonEnabled);
-        }}>
-        <Text style={{color: 'white', fontWeight: '700'}}>RESET</Text>
-      </TouchableOpacity>
+      <Text>Your first name is {name.firstName}</Text>
+      <Text>Your last name is {name.lastName}</Text>
     </View>
   );
 }
