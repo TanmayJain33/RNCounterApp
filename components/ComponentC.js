bfe8614a951d531035371a875240573dc5c36821
TanmayJain33 <tanmay.jain@crownstack.com>
2022-04-07 15:13:12 +0530

    Add useContext() hook for accessing context data

--- components/ComponentC.js
+++ components/ComponentC.js
@@ -1,25 +1,16 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {View, Text} from 'react-native';
 import {UserContext, AgeContext} from '../App';
 
 export default function ComponentC() {
+  const user = useContext(UserContext);
+  const age = useContext(AgeContext);
+
   return (
     <View>
-      <UserContext.Consumer>
-        {user => {
-          return (
-            <AgeContext.Consumer>
-              {age => {
-                return (
-                  <Text>
-                    User context value is {user} and Age context value is {age}
-                  </Text>
-                );
-              }}
-            </AgeContext.Consumer>
-          );
-        }}
-      </UserContext.Consumer>
+      <Text>
+        {user} - {age}
+      </Text>
     </View>
   );
 }
