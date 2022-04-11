b7fe4c84e0a53ffdf56f64a52ec2b380ac4c5c6e
TanmayJain33 <tanmay.jain@crownstack.com>
2022-04-11 13:09:17 +0530

    Add useReducer() with state and action objects

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
 import React, {useReducer} from 'react';
 import {View, Text, Button} from 'react-native';
 
-const initialState = 0;
+const initialState = {
+  firstCounter: 0,
+  secondCounter: 10,
+};
 
 const reducerFunction = (currentState, action) => {
-  switch (action) {
+  switch (action.type) {
     case 'increment':
-      return currentState + 1;
+      return {
+        ...currentState,
+        firstCounter: currentState.firstCounter + action.value,
+      };
     case 'decrement':
-      return currentState - 1;
+      return {
+        ...currentState,
+        firstCounter: currentState.firstCounter - action.value,
+      };
+    case 'increment2':
+      return {
+        ...currentState,
+        secondCounter: currentState.secondCounter + action.value,
+      };
+    case 'decrement2':
+      return {
+        ...currentState,
+        secondCounter: currentState.secondCounter - action.value,
+      };
     case 'reset':
       return initialState;
     default:
@@ -21,12 +40,44 @@ export default function App() {
 
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{textAlign: 'center', fontSize: 50}}>{counter}</Text>
-      <Button title="Increment" onPress={() => dispatch('increment')} />
+      <Text style={{textAlign: 'center', fontSize: 40}}>
+        Counter 1 - {counter.firstCounter}
+      </Text>
+      <Text style={{textAlign: 'center', fontSize: 40}}>
+        Counter 2 - {counter.secondCounter}
+      </Text>
       <View style={{margin: 10}} />
-      <Button title="Decrement" onPress={() => dispatch('decrement')} />
+      <Button
+        title="Increment Counter 1"
+        onPress={() => dispatch({type: 'increment', value: 1})}
+      />
       <View style={{margin: 10}} />
-      <Button title="Reset" onPress={() => dispatch('reset')} />
+      <Button
+        title="Decrement Counter 1"
+        onPress={() => dispatch({type: 'decrement', value: 1})}
+      />
+      <View style={{margin: 10}} />
+      <Button
+        title="Increment 5"
+        onPress={() => dispatch({type: 'increment', value: 5})}
+      />
+      <View style={{margin: 10}} />
+      <Button
+        title="Decrement 5"
+        onPress={() => dispatch({type: 'decrement', value: 5})}
+      />
+      <View style={{flexDirection: 'row', margin: 20}}>
+        <Button
+          title="Increment Counter 2"
+          onPress={() => dispatch({type: 'increment2', value: 1})}
+        />
+        <View style={{margin: 10}} />
+        <Button
+          title="Decrement Counter 2"
+          onPress={() => dispatch({type: 'decrement2', value: 1})}
+        />
+      </View>
+      <Button title="Reset" onPress={() => dispatch({type: 'reset'})} />
     </View>
   );
 }
