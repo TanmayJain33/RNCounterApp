a8a9c1fcc6d17a259ead86a310c9eb1028682cc0
TanmayJain33 <tanmay.jain@crownstack.com>
2022-04-12 16:59:41 +0530

    Add useCallback() hook

--- App.js
+++ App.js
@@ -1,38 +1,11 @@
-import React, {useReducer, createContext} from 'react';
-import ComponentA from './components/ComponentA';
-import ComponentB from './components/ComponentB';
-import ComponentC from './components/ComponentC';
-import {View, Text} from 'react-native';
-
-export const CounterContext = createContext();
-
-const initialState = 0;
-
-const reducerFunction = (currentState, action) => {
-  switch (action) {
-    case 'increment':
-      return currentState + 1;
-    case 'decrement':
-      return currentState - 1;
-    case 'reset':
-      return initialState;
-    default:
-      return currentState;
-  }
-};
+import React from 'react';
+import {View} from 'react-native';
+import ParentComponent from './components/ParentComponent';
 
 export default function App() {
-  const [counter, dispatch] = useReducer(reducerFunction, initialState);
-
   return (
-    <CounterContext.Provider
-      value={{counterState: counter, counterDispatch: dispatch}}>
-      <View style={{flex: 1, alignItems: 'stretch', justifyContent: 'center'}}>
-        <Text style={{fontSize: 50, textAlign: 'center'}}>{counter}</Text>
-        <ComponentA />
-        <ComponentB />
-        <ComponentC />
-      </View>
-    </CounterContext.Provider>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <ParentComponent />
+    </View>
   );
 }
